8ac15fca2e1e12c4263e0535b1cfe521875cb526
Sergio <sergiocelemin1998@gmail.com>
2020-08-07 18:22:04 -0500

    form component completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Formulario from "./components/Formulario";
 function App() {
+  //state de todas las citas
+  const [citas, guardarCitas] = useState([]);
+
+  const crearCita = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
+
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Formulario />
+            <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column"> 2 </div>
         </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const Formulario = () => {
+const Formulario = ({ crearCita }) => {
   const [cita, actualizarCita] = useState({
     mascota: "",
     propietario: "",
@@ -9,8 +10,42 @@ const Formulario = () => {
     sintomas: "",
   });
 
+  const [error, actualizarError] = useState(false);
+
+  //Manejar cambios en los input
   const handleChange = (e) => {
     actualizarCita({ ...cita, [e.target.name]: e.target.value });
+  };
+
+  //evento Submit form
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+    actualizarError(false);
+    //asignar id
+    cita.id = uuidv4();
+
+    //Crear cita
+    crearCita(cita);
+    //restart form
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
   };
 
   //get values
@@ -19,7 +54,10 @@ const Formulario = () => {
   return (
     <Fragment>
       <h2>Crear cita</h2>
-      <form>
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios amigo</p>
+      ) : null}
+      <form onSubmit={submitCita}>
         <label>Nombre Mascota</label>
         <input
           type="text"
